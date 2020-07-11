329b147b73d1e58402c80497f89a7a3ba350e9b0
Roger Zavala <rogeliozavala15@gmail.com>
2020-07-11 11:57:23 -0700

    pusing project

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const Story = require('../models/Story');
+const {ensureAuth, ensureGuest} = require('../middleware/auth');
+
+// login/Landing page
+router.get('/', ensureGuest, (req, res) => {
+    res.render('login', {
+        layout: 'login'
+    })
+})
+
+// Dashboard
+router.get('/dashboard', ensureAuth, async(req, res) => {
+    try{
+        const stories = await Story.find({ user: req.user.id}).lean()
+        res.render('dashboard', {
+            name: req.user.firstName,
+            stories
+        })
+    } catch(err){
+        console.log(err)
+        res.render('error/500');
+    }
+
+
+   
+})
+
+module.exports = router;
